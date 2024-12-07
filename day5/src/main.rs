7517a8a34a6fe1976bfcb9eaa9c9ee9058b7a465
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-12-07 14:39:24 +0100

    Fixes part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    println!("Hello, day 5!");
+
+    let args: Vec<String> = env::args().collect();
+    let config = day5::Config::build(&args)?;
+    let middle_page_sum = day5::run(config)?;
+
+    println!("Middle page sum:         {middle_page_sum}");
+    Ok(())
 }

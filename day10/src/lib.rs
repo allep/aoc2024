a9776e4b7cdeb857b18d2dddd391714361a51671
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-12-12 15:10:00 +0100

    WIP

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,55 +1,30 @@
 use csv::Reader;
 use serde::de::DeserializeOwned;
+use std::fs;
 use std::io::{self, Read};
 use std::{error::Error, fs::File, process};
 
-#[derive(Debug, serde::Deserialize)]
-struct Entry {
-    output_start: i32,
-    input_start: i32,
-    input_range: i32,
-}
-
 #[derive(Debug)]
 pub struct Config {
-    first_file: String,
-    second_file: String,
+    puzzle_input: String,
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
+        if args.len() < 2 {
             return Err("Not enough arguments");
         }
 
-        let first_file = args[1].clone();
-        let second_file = args[2].clone();
+        let puzzle_input = args[1].clone();
 
-        Ok(Config {
-            first_file,
-            second_file,
-        })
+        Ok(Config { puzzle_input })
     }
 }
 
-fn deserialize<T, R>(reader: R) -> Result<Vec<T>, Box<dyn std::error::Error>>
-where
-    T: std::fmt::Debug + DeserializeOwned,
-    R: Read,
-{
-    let mut rdr = Reader::from_reader(reader);
-    let mut structs: Vec<T> = Vec::new();
-    for result in rdr.deserialize() {
-        let record: T = result?;
-        structs.push(record);
-    }
-
-    Ok(structs)
-}
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // TODO
-    Ok(())
+pub fn run(config: Config) -> Result<(u32), Box<dyn Error>> {
+    let raw_content = fs::read_to_string(config.puzzle_input)?;
+    let result: u32 = 0;
+    Ok((result))
 }
 
 // Note on printing during tests:
@@ -63,24 +38,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn proper_deserialize_from_slice_to_entry() {
-        // Note: must be without spaces
-        let data = "\
-output_start,input_start,input_range
-50,98,2
-52,50,48
-";
-
-        let structs: Vec<Entry> = deserialize(data.as_bytes()).unwrap();
-    }
-
-    #[test]
-    fn proper_deserialize_from_file_to_entry() {
-        // Note: must be without spaces and by default the base directory should be at the same
-        // level of src
-        let file = File::open("content/sample-content.csv").unwrap();
-        let reader = BufReader::new(file);
-
-        let structs: Vec<Entry> = deserialize(reader).unwrap();
+    fn count_trailhead_test() {
+        todo!();
     }
 }
--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, error};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    println!("Hello, day 10!");
+
+    let args: Vec<String> = env::args().collect();
+    let config = day10::Config::build(&args)?;
+    let (result) = day10::run(config)?;
+
+    println!("Total sum of scores:         {result}");
+    Ok(())
 }

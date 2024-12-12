13d4a7753612a9933f95ec49b835aa70de0e3dfa
Alessandro Paganelli <alessandro.paganelli@zuru.com>
2024-12-12 15:28:47 +0100

    WIP: adds more test logic

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -21,6 +21,55 @@ impl Config {
     }
 }
 
+struct TopographicMap {
+    positions: Vec<Vec<char>>,
+    x_max: usize,
+    y_max: usize,
+    trailheads: Vec<(usize, usize)>,
+}
+
+impl TopographicMap {
+    pub fn make(raw_data: &str) -> Result<TopographicMap, &'static str> {
+        let positions: Vec<Vec<char>> = raw_data
+            .trim()
+            .split("\n")
+            .map(|s| s.chars().collect())
+            .collect();
+
+        if positions.len() == 0 {
+            return Err("No lines read from raw content.");
+        }
+
+        if positions[0].len() == 0 {
+            return Err("Read empty line.");
+        }
+
+        let y_max = positions.len();
+        let x_max = positions[0].len();
+
+        Ok(TopographicMap {
+            positions,
+            x_max,
+            y_max,
+            trailheads: Vec::new(),
+        })
+    }
+
+    fn compute_trailheads(&mut self) {
+        for (y, line) in self.positions.iter().enumerate() {
+            for (x, c) in line.iter().enumerate() {
+                if *c == '0' {
+                    self.trailheads.push((x, y));
+                }
+            }
+        }
+    }
+
+    fn trailheads_num(&self) -> usize {
+        self.trailheads.len()
+    }
+}
+
 pub fn run(config: Config) -> Result<(u32), Box<dyn Error>> {
     let raw_content = fs::read_to_string(config.puzzle_input)?;
     let result: u32 = 0;
@@ -39,6 +88,19 @@ mod tests {
 
     #[test]
     fn count_trailhead_test() {
-        todo!();
+        let data = "
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
+
+        let mut topographic_map = TopographicMap::make(data).unwrap();
+        topographic_map.compute_trailheads();
+        let num_trailheads = topographic_map.trailheads_num();
+        assert_eq!(num_trailheads, 9);
     }
 }

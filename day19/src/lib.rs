92537631bb9b69df2394f2665a2c335f2b198045
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2024-12-26 20:38:04 +0100

    WIP: initial setup of tests

---
+++ day19/src/lib.rs
@@ -0,0 +1,73 @@
+use csv::Reader;
+use serde::de::DeserializeOwned;
+use std::collections::HashSet;
+use std::io::{self, Read};
+use std::{error::Error, fs::File, process};
+
+#[derive(Debug)]
+pub struct Config {
+    puzzle_input_dictionary: String,
+    puzzle_input_combinations: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let puzzle_input_dictionary = args[1].clone();
+        let puzzle_input_combinations = args[2].clone();
+
+        Ok(Config {
+            puzzle_input_dictionary,
+            puzzle_input_combinations,
+        })
+    }
+}
+
+pub fn parse_dictionary_from_raw(raw_dic: &str) -> HashSet<String> {
+    raw_dic.split(',').map(|t| t.trim().to_owned()).collect()
+}
+
+pub fn parse_candidates_from_raw(raw_candidates: &str) -> Vec<String> {
+    raw_candidates.lines().map(|s| s.to_owned()).collect()
+}
+
+pub fn run(config: Config) -> Result<usize, Box<dyn Error>> {
+    // TODO
+    Ok(0)
+}
+
+// Note on printing during tests:
+// - Run test sequentially in case of need with: cargo test -- --test-threads 1
+// - Do not capture test output for debug with: cargo test -- --nocapture
+
+#[cfg(test)]
+mod tests {
+    use io::BufReader;
+
+    use super::*;
+
+    #[test]
+    fn sample_input_test() {
+        let dictionary = "\
+r, wr, b, g, bwu, rb, gb, br";
+        let candidates = "\
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+
+        let dictionary = parse_dictionary_from_raw(dictionary);
+        assert_eq!(dictionary.len(), 8);
+        dbg!(&dictionary);
+        let candidates = parse_candidates_from_raw(candidates);
+        assert_eq!(candidates.len(), 8);
+        dbg!(&candidates);
+    }
+}
